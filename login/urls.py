aa918c83be6b3dcfadc81ac85c9253399148f9f4
SanifKandel <sanifkandel12345@gmail.com>
2022-05-15 19:29:04 +0545

    Updates

---
+++ login/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+from . import views
+
+urlpatterns = [
+
+    path('login/',views.loginProcess,name="login"),
+    path('index/',views.HomeProcess,name="index"),
+
+   
+]
