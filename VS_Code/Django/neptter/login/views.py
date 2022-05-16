dbcb28d82fe83447797cc985f6cebc2f65be115e
SanifKandel <sanifkandel12345@gmail.com>
2022-05-16 12:28:27 +0545

    Rough Update on Login

---
+++ VS_Code/Django/neptter/login/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate,logout
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+def loginProcess(request):
+    # if request.user.is_authenticated:
+    #     return redirect ('login')
+    
+    if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request,username=username, password=password)
+
+            if username and password !="":
+                if user is not None:
+                    auth_login(request,user)
+                    return redirect('index')
+                else:
+                    messages.info(request, "Username or password is incorrect")
+            else:
+                messages.info(request, "Enter username and password")
+    else:
+            return render(request, 'login.html')
+
+def HomeProcess(request):
+    return render(request, 'hello.html')
