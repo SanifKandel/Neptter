e3597a1e973704695fdbdd3299c9b9d6aceab447
SanifKandel <sanifkandel12345@gmail.com>
2022-05-16 14:34:11 +0545

    Updated test

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import Client, SimpleTestCase,TestCase
+from django.urls import resolve, reverse
+
+from login.views import loginProcess
 
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    def test_case_login_url(self):
+        url=reverse('login')
+        print(url)
+        self.assertEqual(resolve(url).func,loginProcess)
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.loginUrl=reverse('login')
+        self.client = Client()
+        self.client.login(username='test' , password='test')
+
+    def test_login(self):
+        response = self.client.get(self.loginUrl)
+        self.assertEqual(response.status_code,200)
