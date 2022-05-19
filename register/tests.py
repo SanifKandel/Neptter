b727c444c46b27a5a7dce36036824028b6b16521
samir571 <sameershrestha68@gmail.com>
2022-05-20 00:11:05 +0545

    Sprint1 testing

--- register/tests.py
+++ register/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import Client, SimpleTestCase,TestCase
+from django.urls import resolve, reverse
+
+from login.views import registerProcess
 
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    def test_case_register_url(self):
+        url=reverse('register')
+        print(url)
+        self.assertEqual(resolve(url).func,registerProcess)
