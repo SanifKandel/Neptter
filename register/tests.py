9217c108ae7fca74c42c05c4426d378a396e0a21
coder-anish <anishkhanal27@gmail.com>
2022-05-21 13:50:00 +0545

    test views and forms

--- register/tests.py
+++ register/tests.py
@@ -1,12 +1,33 @@
 from django.test import Client, SimpleTestCase,TestCase
 from django.urls import resolve, reverse
 
-from login.views import registerProcess
+# from login.views import registerProcess
+from .forms import *
 
 # Create your tests here.
 
-class TestUrls(SimpleTestCase):
-    def test_case_register_url(self):
-        url=reverse('register')
-        print(url)
-        self.assertEqual(resolve(url).func,registerProcess)
+class TestForm(TestCase):
+    def test_register_form_valid_data(self):
+        valid_form = CreateUserForm(data={
+            'username': 'test',
+            'email': 'test@test.com',
+            'first_name': 'test',
+            'password1': 'test@123',
+            'password2': 'test@123',
+        })
+        print(valid_form.errors)
+        self.assertTrue(valid_form.is_valid())
+
+class TestViews(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = Client()
+        cls.register_page_url = reverse('register')
+
+    def setUp(self):
+        pass
+
+    def test_register_page(self):
+        response = self.client.get(self.register_page_url)
+        self.assertEquals(response.status_code, 200)
