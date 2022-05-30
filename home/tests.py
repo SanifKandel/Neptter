39637a15cea57ad30d791fd367543439da1f50da
SanifKandel <sanifkandel12345@gmail.com>
2022-05-30 14:23:59 +0545

    Sprint3 Url Test

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import Client, SimpleTestCase,TestCase
+from django.urls import resolve, reverse
+
+from home.views import HomeProcess
 
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+    def test_case_home_url(self):
+        url=reverse('home')
+        print(url)
+        self.assertEqual(resolve(url).func,HomeProcess)
