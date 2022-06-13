9d6e7b25bbee4e07468403b609baecdc32223beb
karma <tanatsundue3@gmail.com>
2022-06-13 11:35:03 +0545

    Admin Backend

---
+++ admins/views.py
@@ -0,0 +1,33 @@
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import render
+
+
+
+# Create your views here.
+from login.auth import admin_only
+
+
+
+
+@login_required
+@admin_only
+def admin_dashboard(request):
+    user = User.objects.filter(is_staff=0)
+    user_count = user.count()
+    admin = User.objects.filter(is_staff=1)
+    admin_count = admin.count()
+
+    user_info = user.filter(is_staff=0)
+    admin_info = admin
+
+    print(admin_info)
+
+    context = {
+        'user': user_count,
+        'admin': admin_count,
+        'user_info': user_info,
+        'admin_info': admin_info
+    }
+    return render(request, 'admin-dashboard.html', context)
